45418a4eb6f652b75d1146374fbadfadf7cafa2d
Joseph Davitt <joseph.j.davitt@gmail.com>
2024-04-22 08:58:24 -0700

    add counter with redux toolkit

---
+++ src/counter/Counter.js
@@ -0,0 +1,21 @@
+
+import { useDispatch, useSelector } from "react-redux";
+import { decrement, increment, incrementByAmount, selectCount } from "./reducer";
+
+
+function Counter() {
+  const count = useSelector(selectCount);
+  const dispatch = useDispatch();
+
+
+
+  return (
+    <div>
+      <button onClick={() => dispatch(increment())}>+</button>
+      <span>{count}</span>
+      <button onClick={() => dispatch(decrement())}>-</button>
+    </div>
+  )
+}
+
+export default Counter;
--- src/store.js
+++ src/store.js
@@ -1,6 +1,11 @@
-import { createStore } from 'redux';
-import reducer from './reducer';
+import { configureStore } from "@reduxjs/toolkit";
+import counterReducer from "./counter/reducer";
 
-const store = createStore(reducer);
+const store = configureStore({ 
+  reducer: {
+    counter: counterReducer 
+  } 
+    
+})
 
 export default store;
