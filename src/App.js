3dc4c4160af4616ac1733da69abb8a8b03ec6d32
Joseph Davitt <joseph.j.davitt@gmail.com>
2024-04-22 09:07:02 -0700

    add second counter

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 
 import './App.css';
 import Counter from './counter/Counter';
+import Counter2 from './counter2/Counter2';
 
 function App() {
   return (
     <div className="App">
      <Counter />
+     <Counter2 />
     </div>
   );
 }
--- src/store.js
+++ src/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import counterReducer from "./counter/reducer";
+import counterReducer2 from './counter2/reducer';
 
 const store = configureStore({ 
   reducer: {
-    counter: counterReducer 
+    counter: counterReducer,
+    counter2: counterReducer2 
   } 
     
 })
